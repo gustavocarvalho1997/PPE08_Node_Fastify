86ad8b7321914221bd2d17dd9056ad023faa4f38
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-02-18 00:59:45 -0300

    add summary, tags e description

--- src/routes/subscribe-to-event-route.ts
+++ src/routes/subscribe-to-event-route.ts
@@ -5,6 +5,9 @@ export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
     // Define a route
     app.post('/subscriptions', {
         schema: {
+            summary: 'Subscribe someone to an event',
+            tags: ['subscriptions'],
+            description: 'Subscribe someone to an event by providing their name and email address',
             body: z.object({
                 name: z.string(),
                 email: z.string().email(),
