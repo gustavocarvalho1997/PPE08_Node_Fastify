85e382962c5e260d69ffabdb736e2afa6a959c6e
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-02-18 00:19:31 -0300

    criado corpo e config de cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
 import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors';
 
+// Create a Fastify instance
 const app = fastify();
 
+// Enable CORS
+app.register(fastifyCors, {
+    origin: true,
+});
+
+// Define a route
+app.get('/hello', () => {
+    return 'Hello World!';
+})
+
+
+// Start the server
 app.listen({port: 3333}).then(() => {
     console.log('HTTP server is running!')
 });
