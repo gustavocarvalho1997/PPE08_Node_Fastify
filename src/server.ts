84471359f035516b71e933e3ae6ee076c4ba6e99
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-02-18 00:25:33 -0300

    configurado compiler

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { fastify } from 'fastify';
 import { fastifyCors } from '@fastify/cors';
+import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod'
 
 // Create a Fastify instance
 const app = fastify();
@@ -8,6 +9,10 @@ const app = fastify();
 app.register(fastifyCors, {
     origin: true,
 });
+
+// Register the compiler
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
 // Define a route
 app.get('/hello', () => {
