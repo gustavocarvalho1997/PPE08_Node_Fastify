8be594be5a7548d3c3be04257a6e05368b2eafc1
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-02-18 23:34:10 -0300

    add funcao a rota e retorna o id da subscription

--- src/routes/subscribe-to-event-route.ts
+++ src/routes/subscribe-to-event-route.ts
@@ -1,32 +1,40 @@
-import { z } from 'zod';
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
+import { z } from 'zod';
+import { subscribeToEvent } from '../functions/subscribe-to-event';
 
-export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
     // Define a route
-    app.post('/subscriptions', {
-        schema: {
-            summary: 'Subscribe someone to an event',
-            tags: ['subscriptions'],
-            description: 'Subscribe someone to an event by providing their name and email address',
-            body: z.object({
-                name: z.string(),
-                email: z.string().email(),
-            }),
-            response: {
-                201: z.object({
+    app.post(
+        '/subscriptions',
+        {
+            schema: {
+                summary: 'Subscribe someone to an event',
+                tags: ['subscriptions'],
+                description:
+                    'Subscribe someone to an event by providing their name and email address',
+                body: z.object({
                     name: z.string(),
-                    email: z.string()
-                })
+                    email: z.string().email(),
+                }),
+                response: {
+                    201: z.object({
+                        subscriberId: z.string(),
+                    }),
+                },
             },
         },
-    }, async (request, reply) => {
-        const { name, email } = request.body;
+        async (request, reply) => {
+            const { name, email } = request.body;
 
-        // Save the subscription to the database
+            // Save the subscription to the database
+            const { subscriberId } = await subscribeToEvent({
+                name,
+                email,
+            });
 
-        return reply.status(201).send({
-            name,
-            email,
-        })
-    })
-}
+            return reply.status(201).send({
+                subscriberId,
+            });
+        }
+    );
+};
