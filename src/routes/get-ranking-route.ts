6423a62a786dcdb0daf278dcd4774c03136b20f1
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-02-20 14:22:15 -0300

    cria a rota para retornar o top 3 usuarios no ranking de indicacoes

---
+++ src/routes/get-ranking-route.ts
@@ -0,0 +1,36 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
+import { z } from 'zod';
+import { getRanking } from '../functions/get-ranking';
+import { getSubscriberInvitesCount } from '../functions/get-subscriber-invites-count';
+
+export const getRankingRoute: FastifyPluginAsyncZod = async app => {
+    // Define a route
+    app.get(
+        '/ranking',
+        {
+            schema: {
+                summary: 'Get ranking',
+                tags: ['referral'],
+                description: 'Get the ranking of the subscribers',
+                response: {
+                    200: z.object({
+                        ranking: z.array(
+                            z.object({
+                                id: z.string(),
+                                name: z.string(),
+                                score: z.number(),
+                            })
+                        ),
+                    }),
+                },
+            },
+        },
+        async request => {
+            const { rankingWithScore } = await getRanking();
+
+            return {
+                ranking: rankingWithScore,
+            };
+        }
+    );
+};
