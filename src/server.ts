af71082b390e057e75d76f13ca551cfe063ad70f
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-02-20 13:01:06 -0300

    registra no server.ts a rota

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,8 @@ import {
     validatorCompiler,
 } from 'fastify-type-provider-zod';
 import { env } from './env';
-import { accessInviteLinkRoute } from './routes/access-invite-link';
+import { accessInviteLinkRoute } from './routes/access-invite-link-route';
+import { getSubscriberInviteClicksRoute } from './routes/get-subscriber-invite-clicks-route';
 import { subscribeToEventRoute } from './routes/subscribe-to-event-route';
 
 // Create a Fastify instance
@@ -43,6 +44,7 @@ app.register(fastifySwaggerUi, {
 // Register the route
 app.register(subscribeToEventRoute);
 app.register(accessInviteLinkRoute);
+app.register(getSubscriberInviteClicksRoute);
 
 // Start the server
 app.listen({ port: env.PORT }).then(() => {
