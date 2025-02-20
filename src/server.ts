77e09e52288f4a57d9fb2c23cd9249b365f4dd6a
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-02-20 13:25:15 -0300

    cadastra a rota

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ import {
 import { env } from './env';
 import { accessInviteLinkRoute } from './routes/access-invite-link-route';
 import { getSubscriberInviteClicksRoute } from './routes/get-subscriber-invite-clicks-route';
+import { getSubscriberInvitesCountRoute } from './routes/get-subscriber-invites-count-route';
 import { subscribeToEventRoute } from './routes/subscribe-to-event-route';
 
 // Create a Fastify instance
@@ -45,6 +46,7 @@ app.register(fastifySwaggerUi, {
 app.register(subscribeToEventRoute);
 app.register(accessInviteLinkRoute);
 app.register(getSubscriberInviteClicksRoute);
+app.register(getSubscriberInvitesCountRoute);
 
 // Start the server
 app.listen({ port: env.PORT }).then(() => {
